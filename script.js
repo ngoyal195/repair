5f7f378b55498092f3ecc0fed1775395070fa276
ngoyal195 <namangoyal195@gmail.com>
2026-03-09 11:51:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,33 +13,27 @@ const sec = String(now.getSeconds()).padStart(2,'0')
 const random = Math.floor(Math.random()*900)+100
 
 return `GMN-${year}${month}${day}-${hour}${min}${sec}-${random}`
-
 }
 
 const ticket = generateTicket()
+
 document.getElementById("ticket").innerText = ticket
+document.getElementById("ticketInput").value = ticket
+
 
 document.getElementById("repairForm").addEventListener("submit", function(e){
 
 e.preventDefault()
 
-const params = {
-
-ticket_id: ticket,
-name: document.getElementById("name").value,
-phone: document.getElementById("phone").value,
-email: document.getElementById("email").value,
-bag_type: document.getElementById("bagType").value,
-warranty: document.getElementById("warranty").value,
-balance: document.getElementById("balance").value,
-issue: document.getElementById("issue").value
-
-}
-
-emailjs.send("service_chf6h93","template_dr655gw",params)
-.then(function(){
+emailjs.sendForm(
+"service_chf6h93",
+"template_dr655gw",
+this
+).then(function(){
 
 alert("Repair ticket submitted successfully.\nTicket ID: " + ticket)
+
+document.getElementById("repairForm").reset()
 
 })
 
